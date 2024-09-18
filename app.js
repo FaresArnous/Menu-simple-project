3089f7a718b9ccea6c03c25c15307ba94b924317
fares Arnous <Faris.Arnous@hotmail.com>
2024-09-18 10:50:18 +0300

    added submite function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const decreaseButtons = document.querySelectorAll(".decrease");
   const increaseButtons = document.querySelectorAll(".increase");
+  const submitButton = document.querySelector("#sub-btn button");
 
   function updateQuantity(itemName, change) {
     const formattedId = itemName.replace(/\s+/g, "-");
@@ -22,5 +23,26 @@ document.addEventListener("DOMContentLoaded", () => {
       const itemName = button.getAttribute("data-item");
       updateQuantity(itemName, 1);
     });
+  });
+
+  submitButton.addEventListener("click", () => {
+    const items = document.querySelectorAll(".item");
+    const order = {};
+
+    items.forEach((item) => {
+      const itemName = item.querySelector("h2").textContent;
+      const formattedId = itemName.replace(/\s+/g, "-");
+      const quantity = document.getElementById(`${formattedId}-quantity`).value;
+
+      if (quantity > 0) {
+        order[itemName] = quantity;
+      }
+    });
+
+    localStorage.setItem("order", JSON.stringify(order));
+
+    alert("Order submitted!");
+
+    console.log("Order Submitted: ", order);
   });
 });
